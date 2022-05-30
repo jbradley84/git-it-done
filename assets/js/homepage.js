1c6c92427c581b79f6950a8bc1e9b1ba4d962a31
jbradley84 <utbradley84@yahoo.com>
2022-05-30 18:38:45 -0500

    added error handling to fetch request

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -10,10 +10,17 @@ var getUserRepos = function (user) {
 
     // make a get request to url
     fetch(apiUrl).then(function (response) {
-        console.log(response);
-        response.json().then(function (data) {
-            displayRepos(data, user);
-        });
+        if (response.ok) {
+            response.json().then(function (data) {
+                displayRepos(data, user);
+            });
+        } else {
+            alert("Error: GitHub User Not Found");
+        }
+    })
+    .catch(function(error) {
+        // Notice this '.catch()' getting chained onto the end of the '.then()' method
+        alert("Unable to connect to GitHub");
     });
 };
 
@@ -33,7 +40,13 @@ var formSubmitHandler = function (event) {
 
 
 // function to display repos
-var displayRepos = function(repos, searchTerm) {
+var displayRepos = function (repos, searchTerm) {
+    // check if api returned any repos
+    if (repos.length === 0) {
+        repoContainerEl.textContent = "No repositories found."
+        return;
+    }
+
     // clear old content
     repoContainerEl.textContent = "";
     repoSearchTerm.textContent = searchTerm;
