05bb400817d5e608366559c660be302504add18f
jbradley84 <utbradley84@yahoo.com>
2022-05-30 17:46:48 -0500

    created js form handler to search github user repositories

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,14 +1,34 @@
-var getUserRepos = function(user) {
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+
+var getUserRepos = function (user) {
     // format the github api url
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
-  
+
     // make a get request to url
-    fetch(apiUrl).then(function(response) {
-      console.log(response);
-      response.json().then(function(data) {
-        console.log(data);
-      });
+    fetch(apiUrl).then(function (response) {
+        console.log(response);
+        response.json().then(function (data) {
+            console.log(data);
+        });
     });
-  };
-  
-  getUserRepos("lernantino");
+};
+
+
+var formSubmitHandler = function (event) {
+    event.preventDefault();
+    // get value from input element
+    var username = nameInputEl.value.trim();
+
+    if (username) {
+        getUserRepos(username);
+        nameInputEl.value = "";
+    } else {
+        alert("Please enter a GitHub username");
+    }
+};
+
+
+
+userFormEl.addEventListener("submit", formSubmitHandler);
